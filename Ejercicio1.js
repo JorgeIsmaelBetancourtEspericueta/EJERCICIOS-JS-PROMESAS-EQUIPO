4718e45ea641ccdaad33857306c2f3db60b4307d
Jorge Betancourt <joisbetancourtes@ittepic.edu.mx>
2025-02-20 09:36:58 -0700

    Ejercicios 1 y 6

---
+++ Ejercicio1.js
@@ -0,0 +1,25 @@
+//Ejercicio 1: Simulación de Descarga de Archivo
+
+function downloadFile(size) {
+  return new Promise((resolve, reject) => {
+    //Esperamos 2 segundos
+    setTimeout(() => {
+      //Verificamos que el archivo sea menor a 500MB
+      if (size <= 500) {
+        resolve("Descarga completada");
+      } else {
+        reject("El archivo es demasiado grande");
+      }
+    }, 2000);
+  });
+}
+
+//Probamos cuando el archivo pesa 500MB
+downloadFile(500)
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
+
+//Probamos cuando el archivo es mayor a 500MB
+downloadFile(600)
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
---
+++ Ejercicio6.js
@@ -0,0 +1,30 @@
+//Ejercicio 6: Verificación de Saldo Bancario
+
+let totalMoney = 5000;
+
+function checkMoney(account, amount) {
+  return new Promise((resolve, reject) => {
+    //Esperamos 2 segundos
+    setTimeout(() => {
+      //Verificamos que el retiro sea menor a nuestro saldo
+      if (amount <= totalMoney) {
+        totalMoney = totalMoney - amount;
+        resolve(`${account}, New total: ${totalMoney}, "Approved transaction"`);
+      } else {
+        reject(
+          `error: "No enough money", Available balance ${totalMoney}, code: 402`
+        );
+      }
+    }, 2000);
+  });
+}
+
+//Promesa cuando el retiro es menor a la cantidad
+checkMoney("415678", "2000")
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
+
+//Promesa cuando el retiro es mayor al saldo
+checkMoney("415678", "6000")
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
